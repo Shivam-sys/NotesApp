3d5ab2df6ab1365fce026fdba3f6dccd9c7aaac8
Shivam Kumar <57852651+Shivam-sys@users.noreply.github.com>
2021-12-19 17:33:26 +0530

    setup jwt for logging in

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -67,4 +67,47 @@ router.post(
   }
 );
 
+//authenticate a user using: POST "/api/auth/login .. no login required
+
+router.post(
+  "/login",
+  [
+    body("email", "Enter valid email").isEmail(),
+    body("password", "Password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    //if errors are there in validating req, return bad request and errors.
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Login with correct credentials" });
+      }
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ error: "Login with correct credentials" });
+      }
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(payload, JWT_SECRET);
+      res.json({ message: "Logged In!", authToken });
+    } catch (error) {
+      console.error(error.message);
+      res
+        .status(500)
+        .send("Some internal server error occured while logging in");
+    }
+  }
+);
 module.exports = router;
