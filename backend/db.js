3586ec4be9e5c8d6839b63bd17de55ea6cd8049d
Shivam Kumar <57852651+Shivam-sys@users.noreply.github.com>
2021-12-19 12:05:48 +0530

    name, mail, passdword validations enabled

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+const mongoose = require("mongoose");
+const mongoURI =
+  "mongodb://localhost:27017/notesapp?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI, ()=>{
-        console.log("Connected to mongo successfully");
-    })
-}
+const connectToMongo = () => {
+  mongoose.connect(mongoURI, () => {
+    console.log("Connected to mongo successfully");
+  });
+};
 
 module.exports = connectToMongo;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,42 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
 //Create a user using: POST "/api/auth" . Doesn't require auth
 
-router.post("/", (req, res) => {
-  const user = User(req.body); //assigning whatever body has been recived as request to user const.
-  user.save(); //to save data in the db
-  console.log(req.body);
-  res.send(req.body);
-});
+router.post(
+  "/",
+  [
+    body("name", "Name must be 3 chars long").isLength({ min: 3 }),
+    body("email", "Enter valid email").isEmail(),
+    body("password", "Password must be 5 chars long").isLength({ min: 5 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+      // return res.send("Error occured");
+    }
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err),
+          res.json({
+            error: "Some error occured!",
+            message: err.keyValue,
+          });
+      });
+
+    // {const user = User(req.body); //assigning whatever body has been recived as request to user const.
+    // user.save(); //to save data in the db
+    // console.log(req.body);} #->remove outer {}
+    // res.send(req.body); //if res.json is already done then no need to use res.send
+  }
+);
 
 module.exports = router;
