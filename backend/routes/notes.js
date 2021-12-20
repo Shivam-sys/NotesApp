82fd73d2a1855e3e02e3be86c5f6d55384044bf1
Shivam Kumar <57852651+Shivam-sys@users.noreply.github.com>
2021-12-20 21:05:58 +0530

    create, fetch and update notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -42,4 +42,47 @@ router.post(
     }
   }
 );
+//ROUTE 6: Update an existing note using: PUT "/api/auth/updatenote", login required
+router.put(
+  "/updatenote/:id",
+  fetchuser,
+  [body("description", "Cannot save a note without description").exists()],
+  async (req, res) => {
+    try {
+      //if errors are there in validating req, return bad request and errors.
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const { title, description, tag } = req.body;
+      const newNote = {};
+      if (title) {
+        newNote.title = title;
+      }
+      if (description) {
+        newNote.description = description;
+      }
+      if (tag) {
+        newNote.tag = tag;
+      }
+      //Find the note to be updated using note id and update it
+      let note = await Note.findById(req.params.id);
+      if (!Note) {
+        return res.status(404).send("Not found!");
+      }
+      if (note.user.toString() !== req.user.id) {
+        return res.status(401).send("Not allowed");
+      }
+      note = await Note.findByIdAndUpdate(
+        req.params.id,
+        { $set: newNote },
+        { new: true }
+      );
+      res.json({ note });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Error, Cannot create a note.");
+    }
+  }
+);
 module.exports = router;
